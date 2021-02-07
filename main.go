bc60bc82fc1f56c0b53e598a9ccf23592564d668
Bill Marquette <bill.marquette@gmail.com>
2021-02-07 16:12:16 -0500

    implement good/bad url counter logic

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"crypto/md5"
 	"encoding/hex"
@@ -38,13 +39,13 @@ func initDb() *sql.DB {
 	return db
 }
 
-func shorten(db *sql.DB, url string) string {
+func shorten(db *sql.DB, gurl string, burl string, gcount int, bcount int) string {
 
 	hasher := md5.New()
-	hasher.Write([]byte(url))
+	hasher.Write([]byte(gurl))
 	slug := hex.EncodeToString(hasher.Sum(nil))[0:8]
 
-	if _, err := db.Exec("INSERT INTO slugs(slug, gurl) VALUES ($1, $2)", slug, url); err != nil {
+	if _, err := db.Exec("INSERT INTO slugs(slug, gurl, burl, gcount, bcount) VALUES ($1, $2, $3, $4, $5)", slug, gurl, burl, gcount, bcount); err != nil {
 		return err.Error()
 	}
 
@@ -52,7 +53,18 @@ func shorten(db *sql.DB, url string) string {
 }
 
 func handleNewSlug(c *gin.Context) {
-	slug := shorten(db, c.PostForm("url"))
+	gurl := c.PostForm("gurl")
+	burl := c.PostForm("burl")
+	gcount, err := strconv.Atoi(c.PostForm("gcount"))
+	if err != nil {
+		gcount = 0
+	}
+	bcount, err := strconv.Atoi(c.PostForm("bcount"))
+	if err != nil {
+		bcount = 0
+	}
+
+	slug := shorten(db, gurl, burl, gcount, bcount)
 
 	c.JSON(http.StatusOK, gin.H{"slug": slug})
 }
@@ -60,7 +72,7 @@ func handleNewSlug(c *gin.Context) {
 func handleGetSlug(c *gin.Context) {
 	//c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
 
-	rows, err := db.Query("SELECT gurl FROM slugs WHERE slug = $1", c.Param("slug"))
+	rows, err := db.Query("SELECT gurl, burl, gcount, bcount FROM slugs WHERE slug = $1", c.Param("slug"))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err.Error())
 		return
@@ -69,13 +81,33 @@ func handleGetSlug(c *gin.Context) {
 
 	for rows.Next() {
 		var (
-			url string
+			gurl   string
+			burl   string
+			gcount int
+			bcount int
 		)
-		if err := rows.Scan(&url); err != nil {
+		if err := rows.Scan(&gurl, &burl, &gcount, &bcount); err != nil {
 			c.JSON(http.StatusInternalServerError, err.Error())
 			return
 		}
-		c.Redirect(http.StatusTemporaryRedirect, url)
+
+		// If we still have 'good' serves left, redirect to a good url
+		if 0 < gcount {
+			gcount--
+			db.Query("UPDATE slugs set gcount = $1 where slug = $2", gcount, c.Param("slug"))
+			c.Redirect(http.StatusTemporaryRedirect, gurl)
+			return
+		}
+		// If we still have 'bad' serves left, redirect to the bad url
+		if 0 < bcount {
+			bcount--
+			db.Query("UPDATE slugs set bcount = $1 where slug = $2", bcount, c.Param("slug"))
+			c.Redirect(http.StatusTemporaryRedirect, burl)
+			return
+		}
+
+		// Fall back to always returning the good url
+		c.Redirect(http.StatusTemporaryRedirect, gurl)
 		return
 	}
 
